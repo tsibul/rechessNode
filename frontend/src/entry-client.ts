983edbf158fe20f95f757a8860308be0d369d376
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-08-04 23:47:14 +0300

    change remove pinia

--- frontend/src/entry-client.ts
+++ frontend/src/entry-client.ts
@@ -1,10 +1,10 @@
 import { createApp } from './main'
 
 // Client-side hydration
-const { app, router, pinia } = await createApp()
-
-// wait until router is ready before mounting to ensure hydration matches
+const { app, router} = await createApp()
+//
+// // wait until router is ready before mounting to ensure hydration matches
 router.isReady().then(() => {
   // Hydrate the app
   app.mount('#app', true) // `true` indicates hydration
-}) 
+})
